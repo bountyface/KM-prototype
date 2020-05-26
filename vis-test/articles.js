ababb9df0e26585ae0d6e084029761b0ff774b0f
Michael Ebneth <michael.ebneth@tu-ilmenau.de>
2020-05-26 15:53:08 +0200

    vis-test regions

--- vis-test/articles.js
+++ vis-test/articles.js
@@ -7,7 +7,7 @@ const articlesArray = [
         content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         tags: [employment, safetyAndSecurity],
         type: policyDocument,
-        region: "Germany"
+        region: germany
     },
     {
         id: 2,
@@ -15,42 +15,71 @@ const articlesArray = [
         content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         tags: [policy, safetyAndSecurity],
         type: informationPlatform,
-        region: "Netherlands"
+        region: unitedKingdom
     },
     {
         id: 3,
         title: "Title 3",
         content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         tags: [policy, transportPlanning],
-        type: guideline
+        type: guideline,
+        region: france
     },
     {
         id: 4,
         title: "Title 4",
         content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         tags: [awarenessRaising, participationCulture, educationAndTraining, genderSmartMobilityMeasures],
-        type: measure
+        type: measure,
+        region: spain
     },
     {
         id: 5,
         title: "Title 5",
         content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         tags: [policy, accessibility, publicTransport],
-        type: campaign
+        type: campaign,
+        region: portugal
     },
     {
         id: 6,
         title: "Title 6",
         content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         tags: [employment, accessibility, publicTransport],
-        type: campaign
+        type: campaign,
+        region: italy
     },
     {
         id: 7,
         title: "Title 7",
         content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         tags: [employment],
-        type: campaign
+        type: campaign,
+        region: romania
+    },
+    {
+        id: 8,
+        title: "Title 8",
+        content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+        tags: [educationAndTraining, transportPlanning, awarenessRaising],
+        type: report,
+        region: sweden
+    },
+    {
+        id: 9,
+        title: "Title 9",
+        content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+        tags: [participationCulture, mobilityData, genderSmartMobilityMeasures],
+        type: policyDocument,
+        region: denmark
+    },
+    {
+        id: 10,
+        title: "Title 10",
+        content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+        tags: [accessibility, safetyAndSecurity, employment],
+        type: measure,
+        region: lithuania
     },
 
 ]
